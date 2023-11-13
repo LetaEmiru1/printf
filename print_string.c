f7e28a5890f9423aceccf0b8990c378fd6cf2af9
Leta Malimo <letaemiruleta@gmail.com>
2023-11-14 00:41:58 +0300

    4th commit

---
+++ print_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_string - print a string.
+ * @iterator: va_list variable.
+ * Return: string legth.
+ */
+
+int print_string(va_list iterator)
+{
+	char *s;
+	int i, len;
+
+	s = va_arg(iterator, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+}
